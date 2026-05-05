ed2a6175effaa52c11de7f50be8b3ff01a677d9f
Liam Guting <liam.guting@skola.taby.se>
2026-05-05 07:41:32 +0000

    Andra versionen

--- main.py
+++ main.py
@@ -12,115 +12,104 @@ Funktioner som ska implementeras:
 
 # === FIGURER ===
 
-def rita_kvadrat(sida, tecken):
+def rita_kvadrat(sida, tecken, farg):
     for i in range(sida):
-        print(tecken*sida)
-
-    """
-    Ritar en fylld kvadrat.
-
-    Parametrar:
-        sida (int): Längden på sidan (antal tecken)
-        tecken (str): Tecknet som används för att rita
-    """
-    # TODO: Implementera funktionen
-    # Tips: for i in range(sida): print(tecken * sida)
+        print(farg + tecken*sida)
     pass
 
-
-def rita_triangel(hojd, tecken):
+def rita_triangel(hojd, tecken, farg):
     for i in range(1, hojd+1):
-        print(tecken*1)
-    """
-    Ritar en rätvinklig triangel.
-
-    Parametrar:
-        hojd (int): Triangelns höjd (antal rader)
-        tecken (str): Tecknet som används för att rita
-    """
-    # TODO: Implementera funktionen
-    # Tips: for i in range(1, hojd + 1): print(tecken * i)
+        print(farg + tecken*i)
     pass
 
 
-def rita_cirkel(radie, tecken):
-    for y in range(-radie, radie):
-        for x in range(-radie, radie):
-            if x*x +y*y <= radie*radie:
-                print(tecken, end=" ")
+def rita_cirkel(radie, tecken, farg):
+    for y in range(-radie, radie + 1):
+        for x in range(-radie, radie + 1):
+            if x*x + y*y <= radie*radie:
+                print(farg + tecken + " ", end="")
             else:
-                print("  ", end=" ")
-            print()
-    """
-    Ritar en cirkel med ASCII-tecken.
-    Använder Pythagoras sats (x² + y² ≤ r²) för att avgöra om en punkt är innanför.
-
-    Parametrar:
-        radie (int): Cirkelns radie
-        tecken (str): Tecknet som används för att rita
-    """
-    # TODO: Implementera funktionen
-    # Tips: Loopa y från -radie till radie
-    #       Loopa x från -radie till radie
-    #       Kolla om x*x + y*y <= radie*radie
-    #       Skriv ut tecken + mellanslag eller två mellanslag
-    #       Använd end=" " för att slippa radbrytning
-    #       Glöm inte print() efter varje y-varv
-    pass
-
-
-def rita_blomma(kronblad, storlek, tecken):
-    for k in range(kronblad):
-        print(f"Kronblad{k+1}")
-        rita_kvadrat(storlek,tecken)
+                print("  ", end="")
         print()
-    """
-    Ritar en enkel blomma genom att kombinera flera kvadrater.
-
-    Parametrar:
-        kronblad (int): Antal kronblad
-        storlek (int): Storleken på varje kronblad
-        tecken (str): Tecknet som används för att rita
-    """
-    # TODO: Implementera funktionen
-    # Tips: En enkel blomma kan vara flera kvadrater bredvid varandra
-    # Exempel: rita_kvadrat(storlek, tecken) flera gånger
     pass
 
+
+def rita_blomma(kronblad, storlek, tecken, farg):
+    for k in range(kronblad):
+        print(farg + f"Kronblad {k+1}")
+        rita_kvadrat(storlek,tecken,farg)
+        print()
+    pass
+
+ROD    = "\033[31m"
+GRON   = "\033[32m"
+GUL    = "\033[33m"
+BLA    = "\033[34m"
+RESET  = "\033[0m"
 
 # === HUVUDPROGRAM ===
 
 def huvudprogram():
     while True:
-        val = input("Välj:")
+        print("Välj färg: 1=Röd, 2=Blå, 3=Gul, 4=Grön")
+        farg_val = input("Färg: ")
+        if farg_val == "1":
+            farg = ROD
+        elif farg_val == "2":
+            farg = BLA
+        elif farg_val == "3":
+            farg = GUL
+        elif farg_val == "4":
+            farg = GRON
+        else:
+            farg = RESET
         print("\n --- ASCII MÖNSTER ---")
         print("1. Rita kvadrat")
         print("2. Rita triangel")
         print("3. Rita cirkel")
         print("4. Rita blomma")
-        print("5. Avsluta")
+        print("5. Rita ihålig kvadrat")
+        print("6. Rita omvänd triangel")
+        print("7. Rita diamant")
+        print("8. Avsluta")
+
+        val = input("Välj:")
         if val == "1":
             sida= int(input("Sida: "))
             tecken = input("Tecken: (t.ex *): ")
-            rita_kvadrat(sida,tecken)
+            rita_kvadrat(sida,tecken, farg)
             pass
         elif val == "2":
             hojd = int(input("Höjd: "))
             tecken = input("Vilket tecken?: ")
-            rita_triangel(hojd,tecken)
+            rita_triangel(hojd,tecken, farg)
             pass
         elif val == "3":
             radie = int(input("Radien: "))
             tecken = input("Ange tecken: ")
-            rita_cirkel(radie,tecken)
+            rita_cirkel(radie,tecken, farg)
             pass
         elif val == "4":
             kronblad = int(input("Hur många kronblad: "))
             storlek  = int(input("Storlek: "))
             tecken = input("Ange ett tecken: ")
-            rita_blomma(kronblad,storlek,tecken)
+            rita_blomma(kronblad,storlek,tecken, farg)
             pass
         elif val == "5":
+            sida = int(input("Sidan: "))
+            tecken = input("Tecken: ")
+            rita_ihalig_kvadrat(sida, tecken, farg)
+            pass
+        elif val == "6":
+            hojd = int(input("Höjden "))
+            tecken = input("Tecken ")
+            rita_omvand_triangel(hojd, tecken, farg)
+            pass
+        elif val == "7":
+            hojd = int(input("Höjden "))
+            tecken = input("Tecken ")
+            rita_diamant(hojd, tecken, farg)
+        elif val == "8":
             print("Hejdå")
         break
     else:
@@ -128,7 +117,12 @@ def huvudprogram():
 
 # === EXTRA FUNKTIONER FÖR UTMANINGAR ===
 
-def rita_ihalig_kvadrat(sida, tecken):
+def rita_ihalig_kvadrat(sida, tecken, farg):
+    for i in range(sida):
+        if i == 0 or i == sida-1:
+            print(farg + tecken * sida)
+        else:
+            print(farg + tecken + " " *(sida-2) + tecken)
     """
     Ritar en ihålig kvadrat (endast kantlinjen).
     """
@@ -138,7 +132,9 @@ def rita_ihalig_kvadrat(sida, tecken):
     pass
 
 
-def rita_omvand_triangel(hojd, tecken):
+def rita_omvand_triangel(hojd, tecken, farg):
+    for i in range(hojd, 0, -1):
+        print(farg + tecken*i)
     """
     Ritar en omvänd triangel (basen upp, spetsen ner).
     """
@@ -147,7 +143,11 @@ def rita_omvand_triangel(hojd, tecken):
     pass
 
 
-def rita_diamant(hojd, tecken):
+def rita_diamant(hojd, tecken, farg):
+    for i in range(1, hojd+1):
+        print(farg + tecken*i)
+    for i in range(hojd-1, 0, -1):
+            print(farg + tecken*i)
     """
     Ritar en diamant (två trianglar som möts).
     """
@@ -157,6 +157,10 @@ def rita_diamant(hojd, tecken):
 
 
 def spara_till_fil(figur_namn, innehall, filnamn="figur.txt"):
+    fil = open(filnamn, "w", encoding="utf=8")
+    fil.write("f==={figur_namn}===\n")
+    fil.write(innehall)
+    fil.close()
     """
     Sparar en ASCII-figur till en textfil.
 
